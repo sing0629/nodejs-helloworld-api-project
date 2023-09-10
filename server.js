1ae814abb33da8baf4a6b320a1f58e2c4cf3762a
codeonedigest <codeonedigest@gmail.com>
2023-09-10 08:42:08 +0530

    Added first file for hello world project

---
+++ server.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const port = process.env.PORT||8080
+const app = express()
+
+app.use(bodyParser.json() , cors())
+ 
+ 
+app.get('/', (req, res) => {
+   res.send('Welcome to Nodejs API Project')
+ })
+
+
+ app.get('/hello', (req, res) => {
+   res.send('Hello World!!')
+ })
+
+app.listen(port, () =>  console.log(`server is up and running ${port}`))
